1a83fab2275b51fe9308de3459f9f6d3feff0faa
Craig Martek <craigmartek@gmail.com>
2013-12-12 20:19:30 -0800

    basic concepts there

--- faucet/views.py
+++ faucet/views.py
@@ -1,17 +1,44 @@
+import logging
 import os
 
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import RequestContext
-from django.views.decorators.http import require_GET
+from django.views.generic import View
 from jsonrpclib import Server
 
+DOGE_AMOUNT = 0.01
 
-@require_GET
-def home(request):
-  return render(request, 'base.html',
-      dictionary={'faucet_addr': os.environ['FAUCET_ADDR']},
-      context_instance=RequestContext(request))
+
+class FreeDoge(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, 'base.html',
+        dictionary={'faucet_addr': os.environ['FAUCET_ADDR']},
+        context_instance=RequestContext(request))
+
+  def post(self, request, *args, **kwargs):
+    dictionary={'faucet_addr': os.environ['FAUCET_ADDR']}
+    try:
+      server = get_rpc_server()
+      send_addr = request.POST.get('addr', '')
+      is_valid_resp = server.validateaddress(send_addr)
+      is_valid = is_valid_resp['isvalid']
+      if is_valid:
+        dictionary['send_addr'] = send_addr
+        remaining_balance = server.getbalance()
+        if remaining_balance and remaining_balance > DOGE_AMOUNT:
+          send_resp = server.sendtoaddress(send_addr, DOGE_AMOUNT)
+          if 'code' in send_resp:
+            dictionary['error'] = send_resp['message']
+          else:
+            dictionary['transaction'] = send_resp
+      else:
+        dictionary['error'] = 'invalid address!'
+    except Exception as e:
+      logging.error('error: ' + str(e))
+      dictionary['error'] = 'couldn\'t connect to dogecoin! :('
+    return render(request, 'base.html', dictionary,
+        context_instance=RequestContext(request))
 
 
 def get_rpc_server():
--- freedoge/urls.py
+++ freedoge/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
+from faucet.views import FreeDoge
+
 urlpatterns = patterns('',
-    url(r'^$', 'faucet.views.home'),
+    url(r'^$', FreeDoge.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
