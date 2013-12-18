b8d28af5342a8c4907a99a43f7dd63e863bf69ca
Craig Martek <craigmartek@gmail.com>
2013-12-18 02:25:05 -0500

    add logging because wtf

--- faucet/views.py
+++ faucet/views.py
@@ -86,6 +86,7 @@ def freedoge(request):
 def get_ip(request):
   x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
   if x_forwarded_for:
+    logging.info('xforwadedfor: %s', x_forwarded_for)
     ip = x_forwarded_for.split(',')[-1]
   else:
     ip = request.META.get('REMOTE_ADDR')
